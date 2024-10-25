141fdb4cae4b4a8653c60f9b78afac4d8fcda0b6
Vitalii Savitskii <visa@ecco.com>
2024-10-25 17:01:50 +0200

    Self-review fixes

--- framework/arcane-framework/src/main/scala/services/storage/models/UploadResult.scala
+++ framework/arcane-framework/src/main/scala/services/storage/models/UploadResult.scala
@@ -3,4 +3,11 @@ package services.storage.models
 
 import java.time.OffsetDateTime
 
-case class UploadResult(name: String, lastModified: OffsetDateTime, contentHash: String)
+/**
+ * The result of uploading a blob to a storage.
+ *
+ * @param name The name of the blob.
+ * @param lastModified The last modified time of the blob.
+ * @param contentHash The hash of the content of the blob.
+ */
+final case class UploadResult(name: String, lastModified: OffsetDateTime, contentHash: String)
--- framework/arcane-framework/src/main/scala/services/storage/models/base/BlobPath.scala
+++ framework/arcane-framework/src/main/scala/services/storage/models/base/BlobPath.scala
@@ -1,6 +1,9 @@
 package com.sneaksanddata.arcane.framework
 package services.storage.models.base
 
+/**
+ * A trait that represents a path to a blob.
+ */
 trait BlobPath {
   /**
    * Converts the path to a HDFS-style path.
