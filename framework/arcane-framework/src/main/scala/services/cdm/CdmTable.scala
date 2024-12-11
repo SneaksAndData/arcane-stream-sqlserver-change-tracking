e02345452919a7dac4063f67dbb80dfdb0f1b21c
George Zubrienko <gzu@ecco.com>
2024-12-11 11:29:21 +0100

    Improve prefix generation

--- framework/arcane-framework/src/main/scala/services/cdm/CdmTable.scala
+++ framework/arcane-framework/src/main/scala/services/cdm/CdmTable.scala
@@ -6,7 +6,7 @@ import models.cdm.{SimpleCdmEntity, given}
 import models.{ArcaneSchema, DataRow}
 import services.storage.models.azure.{AdlsStoragePath, AzureBlobStorageReader}
 
-import java.time.{OffsetDateTime, ZoneOffset}
+import java.time.{Instant, OffsetDateTime, ZoneOffset}
 import scala.concurrent.Future
 
 class CdmTable(name: String, storagePath: AdlsStoragePath, entityModel: SimpleCdmEntity, reader: AzureBlobStorageReader):
@@ -14,28 +14,37 @@ class CdmTable(name: String, storagePath: AdlsStoragePath, entityModel: SimpleCd
   private val defaultFromYears: Int = 5
   private val schema: ArcaneSchema = implicitly(entityModel)
 
-  private def getListPrefixes(fromYears: Option[Int]): IndexedSeq[String] =
+  private def getListPrefixes(startDate: Option[OffsetDateTime]): IndexedSeq[String] =
     val currentMoment = OffsetDateTime.now(ZoneOffset.UTC)
-    val fromMoment = currentMoment.minusYears(fromYears.getOrElse(defaultFromYears))
+    val startMoment = startDate.getOrElse(currentMoment.minusYears(defaultFromYears))
     Range.inclusive(
-      fromMoment.getYear,
+      startMoment.getYear,
       currentMoment.getYear
     ).flatMap(year => Range.inclusive(
       1,
       12
-    ).map{ m =>
+    ).map { m =>
       val mon = s"00$m".takeRight(2)
-      s"$year-$mon-"
-    })
+      (s"$year-$mon-", year, m)
+    }).collect {
+      // include all prefixes from previous years
+      // in case year for both dates is the same, we will never hit this case
+      case (prefix, year, _) if year < currentMoment.getYear => prefix
+      // only include prefixes for current year that are less than current month
+      // this only applies to the case when startMoment year is less than current moment - then we take months from 1 to current month
+      case (prefix, year, mon) if (year == currentMoment.getYear) && (currentMoment.getYear > startMoment.getYear) && (mon <= currentMoment.getMonth.getValue) => prefix
+      // in case both dates are in the same year, we limit month selection to start from startMoment month
+      case (prefix, year, mon) if (year == currentMoment.getYear) && (currentMoment.getYear == startMoment.getYear) && (mon >= startMoment.getMonth.getValue) && (mon <= currentMoment.getMonth.getValue) => prefix
+    }
 
   /**
    * Read a table snapshot, taking optional start time.
-   * @param fromYears Folders from Synapse export to include in the snapshot. If not provided, ALL folders will be included
+   * @param startDate Folders from Synapse export to include in the snapshot, based on the start date provided. If not provided, ALL folders from now - defaultFromYears will be included
    * @return A stream of rows for this table
    */
-  def snapshot(fromYears: Option[Int]): Future[LazyList[DataRow]] =
+  def snapshot(startDate: Option[OffsetDateTime] = None): Future[LazyList[DataRow]] =
     // list all matching blobs
-    Future.sequence(getListPrefixes(fromYears)
+    Future.sequence(getListPrefixes(startDate)
       .flatMap(prefix => reader.listBlobs(storagePath + prefix))
       .map(blob => reader.getBlobContent(storagePath + blob.name, _.map(_.toChar).mkString)))
       .map(_.flatMap(content => content.split('\n').foldLeft((Seq.empty[String], "")) { (agg, value) =>
