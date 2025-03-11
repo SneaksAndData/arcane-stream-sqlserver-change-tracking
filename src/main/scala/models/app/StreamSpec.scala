a14555fe62528383070e61517a6591b9f82597cb
Vitalii Savitskii <visa@ecco.com>
2025-03-11 16:53:25 +0100

    Bump the arcane-framework version

--- src/main/scala/models/app/StreamSpec.scala
+++ src/main/scala/models/app/StreamSpec.scala
@@ -1,10 +1,46 @@
 package com.sneaksanddata.arcane.sql_server_change_tracking
 package models.app
 
+import upickle.default.*
+
 /**
   * The configuration of Iceberg sink.
  */
-case class CatalogSettings(namespace: String, warehouse: String, catalogUri: String)
+case class CatalogSettings(namespace: String,
+                           warehouse: String,
+                           catalogUri: String,
+                           catalogName: String,
+                           schemaName: String)derives ReadWriter
+
+case class StagingDataSettingsSpec(tableNamePrefix: String, catalog: CatalogSettings, dataLocation: Option[String]) derives ReadWriter
+
+/**
+ * The configuration of Iceberg sink.
+ */
+case class OptimizeSettingsSpec(batchThreshold: Int,
+                                fileSizeThreshold: String) derives ReadWriter
+/**
+ * The configuration of Iceberg sink.
+ */
+case class SnapshotExpirationSettingsSpec(batchThreshold: Int,
+                                          retentionThreshold: String) derives ReadWriter
+/**
+ * The configuration of Iceberg sink.
+ */
+case class OrphanFilesExpirationSettings(batchThreshold: Int,
+                                         retentionThreshold: String) derives ReadWriter
+/**
+ * The configuration of Iceberg sink.
+ */
+case class SinkSettings(targetTableName: String,
+                        archiveTableName: String,
+                        optimizeSettings: OptimizeSettingsSpec,
+                        snapshotExpirationSettings: SnapshotExpirationSettingsSpec,
+                        orphanFilesExpirationSettings: OrphanFilesExpirationSettings) derives ReadWriter
+
+case class TablePropertiesSettingsSpec(partitionExpressions: Array[String], sortedBy: Array[String], parquetBloomFilterColumns: Array[String], format: String) derives ReadWriter
+
+case class FieldSelectionRuleSpec(ruleType: String, fields: Array[String])derives ReadWriter
 
 /**
  * The specification for the stream.
@@ -31,10 +67,20 @@ case class StreamSpec(database: String,
                       changeCaptureIntervalSeconds: Int,
 
                       // Iceberg settings
+                      stagingDataSettings: StagingDataSettingsSpec,
                       catalogSettings: CatalogSettings,
 
+                      sinkSettings: SinkSettings,
+                      backfillStartDate: String,
+                      backfillBehavior: String,
+                      fieldSelectionRule: FieldSelectionRuleSpec,
+                      tableProperties: TablePropertiesSettingsSpec,
                       stagingLocation: Option[String],
                       sinkLocation: String,
-                      partitionExpression: Option[String])
+                      partitionExpression: Option[String]) derives ReadWriter
 
 
+object StreamSpec:
+
+  def fromEnvironment(envVarName: String): Option[StreamSpec] =
+    sys.env.get(envVarName).map(env => read[StreamSpec](env))
