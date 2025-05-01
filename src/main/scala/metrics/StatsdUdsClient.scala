0dc2a146a0aa29a80e0bee94a6a74a6f337189bc
Vitalii Savitskii <visa@ecco.com>
2025-05-01 16:46:35 +0200

    Use the correct socket type

--- src/main/scala/metrics/StatsdUdsClient.scala
+++ src/main/scala/metrics/StatsdUdsClient.scala
@@ -1,29 +1,35 @@
 package com.sneaksanddata.arcane.sql_server_change_tracking
 package metrics
 
-import jnr.unixsocket.{UnixSocketAddress, UnixSocketChannel}
+import jnr.unixsocket.{UnixDatagramChannel, UnixSocketAddress, UnixSocketChannel}
 import zio.*
 import zio.metrics.connectors.statsd.{StatsdClient, StatsdConfig}
 
-import java.net.{InetSocketAddress, SocketAddress, UnixDomainSocketAddress}
+import java.net.{InetSocketAddress, ProtocolFamily, SocketAddress, StandardProtocolFamily, UnixDomainSocketAddress}
 import java.nio.ByteBuffer
 import java.nio.channels.{DatagramChannel, SocketChannel, WritableByteChannel}
 import scala.util.{Failure, Success, Try}
 import zio.metrics.connectors.statsd.StatsdUdsConfig
 
-class StatsdUdsClient(channel: DatagramChannel, address: SocketAddress) extends StatsdClient:
+import java.nio.channels.spi.SelectorProvider
+
+class StatsdUdsClient(channel: UnixDatagramChannel, address: SocketAddress) extends StatsdClient:
 
   override def send(chunk: Chunk[Byte]): Long = write(chunk.toArray)
 
   private def write(ab: Array[Byte]): Long =
     Try(channel.send(ByteBuffer.wrap(ab), address).toLong) match {
-      case Success(value) => value
-      case Failure(_) => 0L
+      case Success(value) =>
+//        System.out.println(value)
+        value
+      case Failure(t) =>
+        t.printStackTrace()
+        0L
     }
 
 object StatsdUdsClient:
 
-  def apply(channel: DatagramChannel, address: SocketAddress): StatsdUdsClient = new StatsdUdsClient(channel, address)
+  def apply(channel: UnixDatagramChannel, address: SocketAddress): StatsdUdsClient = new StatsdUdsClient(channel, address)
 
   type Environmet = StatsdConfig
 
@@ -37,10 +43,13 @@ object StatsdUdsClient:
     yield new StatsdUdsClient(channel, address)
   }
 
-  private def channelUDS(path: String): ZIO[Any, Throwable, (DatagramChannel, SocketAddress)] =
+  private def channelUDS(path: String): ZIO[Any, Throwable, (UnixDatagramChannel, SocketAddress)] =
     ZIO.attempt {
-      val ch = DatagramChannel.open()
+      val ch = UnixDatagramChannel.open()
       ch.configureBlocking(false)
-      val address: SocketAddress = new UnixSocketAddress(path)
+//      ch.connect(UnixDomainSocketAddress.of(path))
+//      ch.connect(new UnixSocketAddress(path))
+
+      val address: SocketAddress = UnixSocketAddress(path)
       (ch, address)
     }
