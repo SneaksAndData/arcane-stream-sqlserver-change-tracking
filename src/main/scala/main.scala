fb66486fa11bc56dfc456d45ae1e2c88ba4ffa27
Vitalii Savitskii <visa@ecco.com>
2025-05-01 11:43:17 +0200

    WIP

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -74,7 +74,7 @@ object main extends ZIOAppDefault {
     ZLayer.succeed(MetricsConfig(Duration.ofMillis(1000))),
     datadog.datadogLayer,
     statsd.statsdClient,
-    ZLayer.succeed(statsd.StatsdUdsConfig("/Users/visa/socket/aSocket.sock")),
+    ZLayer.succeed(statsd.StatsdUdsConfig(sys.env.getOrElse("SOCKET_PATH", "/tmp/arcane.sock"))),
   )
 
   @main
